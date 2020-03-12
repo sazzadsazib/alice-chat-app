e4a64fb776431c6978a5822ad39286dac31377e3
Sazzad Sazib <sazib66@gmail.com>
2020-03-12 12:54:06 +0600

    feat: updated get started page

---
+++ resources.js
@@ -0,0 +1,17 @@
+export const color  = {
+  primary: '#007B65',
+  primary2: '#007462',
+  primary3: '#007364',
+  primaryDark: '#006865',
+
+  lightGray: '#CCCCCC',
+  offWhite: '#EDF2F4',
+
+  secondary: '#0B132B',
+  secondary2: '#2B2D42',
+  secondary3: '#484B69',
+  secondary4: '#8D99AE',
+
+  white: '#ffffff',
+  black: '#000000',
+}
--- src/get-started/getStarted.js
+++ src/get-started/getStarted.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {color} from './../../resources';
 import {
   View,
   StyleSheet,
@@ -6,6 +7,7 @@ import {
   Dimensions,
   Image,
   Text,
+  TouchableOpacity
 } from 'react-native';
 
 const GetStarted = ({navigation}) => {
@@ -14,22 +16,68 @@ const GetStarted = ({navigation}) => {
 
   return (
     <SafeAreaView>
-      <View>
+      <View style={{height: y}}>
         <Image
           style={{height: y / 2.5, width: x}}
           source={require('./images/drawable-xxxhdpi/bg.png')}
         />
+       <View style={styles.centerContainer}>
+         <Image
+           style={styles.logo}
+           source={require('./images/drawable-xxxhdpi/alicelogo.png')}
+         />
+         <Text style={styles.description}>Manage your customer fast & simple with our CRM</Text>
+       </View>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.actionButton}
+            onPress={()=>navigation.navigate('Login')}
+          >
+            <Text style={styles.actionButtonText}> Get Started </Text>
+          </TouchableOpacity>
+
       </View>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 19,
-    fontWeight: 'bold',
+  centerContainer: {
+    display: 'flex',
+    justifyContent: 'center'
   },
-  activeTitle: {
-    color: 'red',
+  logo: {
+    width: '30%',
+    height: undefined,
+    aspectRatio: 2.902,
+    marginTop: 40,
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
+  description: {
+    color: color.secondary4,
+    fontWeight: "500",
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+    width: '70%',
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  actionButton: {
+    width: '90%',
+    backgroundColor: color.primary,
+    padding: 20,
+    position: 'absolute',
+    bottom: 80,
+    margin: '5%',
+    borderRadius: 10,
+  },
+  actionButtonText: {
+    color: color.white,
+    textAlign: 'center',
+    fontSize: 20,
+  }
 });
 export default GetStarted;
